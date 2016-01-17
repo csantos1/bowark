09194c1e7cae2a2971017aafb5808a46cabe04e0
Cecilia Santos <ceciliasantos@ceciliasantos.com>
2016-01-17 14:59:22 -0200

    adds social media icons to footer

--- footer.php
+++ footer.php
@@ -21,6 +21,7 @@
                     <h1>Join Us</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit In sed egestas urna</p>
                     <?php echo do_shortcode( '[contact-form-7 id="26" title="Join"]' ); ?>
+                    <p><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter-square"></i></a><a href="#"><i class="fa fa-google-plus-square"></i></a><a href="#"><i class="fa fa-linkedin-square"></i></a><a href="#"><i class="fa fa-pinterest-square"></i></a></p>
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bowark' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'bowark' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'bowark' ), 'bowark', '<a href="http://ceciliasantos.com" rel="designer">Cecilia Santos</a>' ); ?>
